746789abb8f812f5e272a7f7ed1ee813d05f1f2b
Justin Stein <justein230@gmail.com>
2020-04-21 12:13:45 -0400

    Added dependencies for package

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/justein230/BbApiConnector-Python",
     packages=setuptools.find_packages(exclude=['resources']),
+    install_requires=[
+        'requests'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
